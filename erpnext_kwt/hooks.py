d927ee923e1ddc413f930939bd49c5dd4c6d2118
Ismail Tabtabai <ismail@tabtabai.org>
2023-09-20 14:15:55 +0300

    Added custom Address fields to fixtures.

--- erpnext_kwt/hooks.py
+++ erpnext_kwt/hooks.py
@@ -10,8 +10,32 @@ app_license = "CC0 1.0 Universal"
 # Includes in <head>
 # ------------------
 
+# Required apps
+required_apps = ["erpnext"]
+
+
 # fixtures
-fixtures = ["KWT Governorate", "KWT District"]
+fixtures = [
+    "KWT Governorate",
+    "KWT District",
+    {"dt": "Custom Field", "filters": [
+        [
+            "name",
+            "in",
+            {
+                "Address-custom_block",
+                "Address-custom_street",
+                "Address-custom_lane",
+                "Address-custom_building",
+                "Address-custom_building_name",
+                "Address-custom_parcel",
+                "Address-custom_floor",
+                "Address-custom_flat",
+                "Address-custom_paci",
+            }
+        ]
+    ]},
+]
 
 
 # include js, css files in header of desk.html
@@ -34,6 +58,9 @@ fixtures = ["KWT Governorate", "KWT District"]
 
 # include js in doctype views
 # doctype_js = {"doctype" : "public/js/doctype.js"}
+doctype_js = {
+    "Address": "public/js/kwt_address.js",
+}
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
 # doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
---
+++ erpnext_kwt/public/js/kwt_address.js
@@ -0,0 +1,9 @@
+frappe.ui.form.on('Address', {
+    refresh(frm) {
+        frm.fields_dict['city'].set_label('District');
+        if (frm.doc.country == "Kuwait") {
+            frm.set_value("city", "Kuwait City");
+            // console.log("Right call");
+        }
+    }
+})
