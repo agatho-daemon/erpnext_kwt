da21281469dcbcaef86c7c097a4466c5f49041a1
Ismail Tabtabai <ismail@tabtabai.org>
2023-11-10 07:01:21 +0300

    Commented City Label change.

--- erpnext_kwt/public/js/kwt_address.js
+++ erpnext_kwt/public/js/kwt_address.js
@@ -1,6 +1,7 @@
 frappe.ui.form.on('Address', {
     refresh(frm) {
-        frm.fields_dict['city'].set_label('District');
+        // frm.fields_dict['city'].set_label('District');
+        // frm..fields_dict['city'].fieldtype = "Link";
         // if (frm.doc.country == "Kuwait") {
         //     frm.set_value("city", "Kuwait City");
         //     // console.log("Right call");
