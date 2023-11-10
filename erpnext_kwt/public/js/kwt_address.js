1dfd547885744310ccff952ab99a03bb3949b339
Ismail Tabtabai <ismail@tabtabai.org>
2023-11-10 07:26:39 +0300

    Hide county, pincode, and state fields in Address form.

--- erpnext_kwt/public/js/kwt_address.js
+++ erpnext_kwt/public/js/kwt_address.js
@@ -1,5 +1,8 @@
 frappe.ui.form.on('Address', {
     refresh(frm) {
+        frm.set_df_property("county", "hidden", 1);
+        frm.set_df_property("pincode", "hidden", 1);
+        frm.set_df_property("state", "hidden", 1);
         // frm.fields_dict['city'].set_label('District');
         // frm..fields_dict['city'].fieldtype = "Link";
         // if (frm.doc.country == "Kuwait") {
