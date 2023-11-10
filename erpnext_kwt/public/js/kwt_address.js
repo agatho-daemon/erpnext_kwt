2857137465554917b73c907290f31077a687e3a7
Ismail Tabtabai <ismail@tabtabai.org>
2023-11-10 07:29:12 +0300

    Hide address line 1 and 2 fields in Address form.

--- erpnext_kwt/public/js/kwt_address.js
+++ erpnext_kwt/public/js/kwt_address.js
@@ -3,6 +3,8 @@ frappe.ui.form.on('Address', {
         frm.set_df_property("county", "hidden", 1);
         frm.set_df_property("pincode", "hidden", 1);
         frm.set_df_property("state", "hidden", 1);
+        frm.set_df_property("address_line1", "hidden", 1);
+        frm.set_df_property("address_line2", "hidden", 1);
         // frm.fields_dict['city'].set_label('District');
         // frm..fields_dict['city'].fieldtype = "Link";
         // if (frm.doc.country == "Kuwait") {
