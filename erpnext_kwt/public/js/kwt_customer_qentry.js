d003542737d1281734ea9c26d3ac87e6a0f212e6
Agathodaemon <31913284+agatho-daemon@users.noreply.github.com>
2023-12-22 08:35:52 +0300

    Update country_changed function in kwt_customer_qentry.js

--- erpnext_kwt/public/js/kwt_customer_qentry.js
+++ erpnext_kwt/public/js/kwt_customer_qentry.js
@@ -1,18 +1,18 @@
 frappe.provide('frappe.ui.form');
 
-// frappe.ui.form.ContactQEntry.prototype.country_changed = function() {
-//     let country = this.dialog.get_value('country');
+frappe.ui.form.ContactQEntry.prototype.country_changed = function() {
+    let country = this.dialog.get_value('country');
 
-//     // Check if the country is Kuwait
-//     if (country === 'Kuwait') {
-//         this.dialog.set_df_property('city', 'label', __('District'));
-//         this.dialog.set_df_property('state', 'label', __('Governorate'));
-//         this.dialog.set_df_property('pincode', 'label', __('PACI'));
-//     } else {
-//         // Reset to default labels if the country is not Kuwait
-//         // Assuming you have default labels stored or you can set them explicitly
-//         this.dialog.set_df_property('city', 'label', __('City'));
-//         this.dialog.set_df_property('state', 'label', __('State'));
-//         this.dialog.set_df_property('pincode', 'label', __('ZIP Code'));
-//     }
-// };
+    // Check if the country is Kuwait
+    if (country === 'Kuwait') {
+        this.dialog.set_df_property('city', 'label', __('District'));
+        this.dialog.set_df_property('state', 'label', __('Governorate'));
+        this.dialog.set_df_property('pincode', 'label', __('PACI'));
+    } else {
+        // Reset to default labels if the country is not Kuwait
+        // Assuming you have default labels stored or you can set them explicitly
+        this.dialog.set_df_property('city', 'label', __('City'));
+        this.dialog.set_df_property('state', 'label', __('State'));
+        this.dialog.set_df_property('pincode', 'label', __('ZIP Code'));
+    }
+};
