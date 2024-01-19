75c34fd93e5600ba5a598ea6832e37ae2fc7ba83
Ismail Tabtabai <ismail@tabtabai.org>
2024-01-19 19:52:55 +0300

    Fixed a need for reload to change field labels based on country value

--- erpnext_kwt/public/js/kwt_customer_qentry.js
+++ erpnext_kwt/public/js/kwt_customer_qentry.js
@@ -8,16 +8,18 @@ frappe.ui.form.ContactQEntry.prototype.render_dialog = function() {
     // Call the original render_dialog function
     original_render_dialog.apply(this);
 
-    // Then apply your custom label logic
-    this.update_field_labels();
+    // Update fields based on country
+    this.update_field_labels(frappe.sys_defaults.country);
 };
 
+
 // Function to update field labels based on the country
-frappe.ui.form.ContactQEntry.prototype.update_field_labels = function() {
-    let country = this.dialog.get_value('country');
+frappe.ui.form.ContactQEntry.prototype.update_field_labels = function(countryValue) {
+    // let country = this.dialog.get_value('country');
+    // console.log('Country changed to:', country);
 
     // Check if the country is Kuwait and update labels
-    if (country === 'Kuwait') {
+    if (countryValue === 'Kuwait') {
         this.dialog.set_df_property('city', 'label', __('District'));
         this.dialog.set_df_property('state', 'label', __('Governorate'));
         this.dialog.set_df_property('pincode', 'label', __('PACI'));
@@ -31,5 +33,7 @@ frappe.ui.form.ContactQEntry.prototype.update_field_labels = function() {
 
 // Extend the country_changed function to use the new label update logic
 frappe.ui.form.ContactQEntry.prototype.country_changed = function() {
-    this.update_field_labels();
+    let countryValue = this.dialog.get_value('country');
+    this.update_field_labels(countryValue);
 };
+
